7a392abbf5715b3ea18d669b0afe484bf9640849
Taehyeon Lee <aaayoo12345@gmail.com>
2026-05-14 18:34:10 +0900

    news 스타일 수정

--- news/src/views/HomeView.vue
+++ news/src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
-    <div 
-      class="oneNews"
-      v-for="item in news" 
-      :key="item.title"
-    >
-      <h3>{{ item.title }}</h3>
-      <p v-html="item.description"></p>
-      <a :href="item.originallink">기사 보러가기</a>
-    </div>
+    <article>
+      <div
+        class="oneNews"
+        v-for="item in news"
+        :key="item.title"
+      >
+        <h3 v-html="item.title"></h3>
+        <small v-html="item.description"></small>
+        <a :href="item.originallink">더 보기</a>
+      </div>
+    </article>
   </div>
 </template>
 
@@ -20,20 +22,37 @@ export default {
   data() {
     return {
       store: useCounterStore(),
-      news: {},
     };
   },
-  mounted() {
-    this.store.getData();
-    this.news = this.store.data.items;
-
-    console.log(this.news);
+  computed: {
+    news() {
+      return this.store.data.items;
+    },
+  },
+  async mounted() {
+    await this.store.getData();
   },
 };
 </script>
 
 <style lang="scss">
   .oneNews {
-    border: 1px solid black;
+    border: 1px solid #42b983;
+    border-radius: 10px;
+    box-shadow: 0 10px 10px 1px rgba($color: #000000, $alpha: 0.2);
+    width: 100%;
+    padding: 10px;
+
+    h3 {
+      margin-bottom: 10px;
+    }
+  }
+  article {
+    max-width: 1000px;
+    display: grid;
+    gap: 20px;
+    text-align: left;
+    margin: 0 auto;
+    padding: 0 20px;
   }
 </style>
